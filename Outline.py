bead0381af0ea3bceefd4e41c867c9d2a8d5f80d
rkoehler <rkoehler@users.noreply.github.com>
2016-04-12 01:35:46 -0400

    Added files via upload

---
+++ Outline.py
@@ -0,0 +1,43 @@
+#Calendar App
+#Author: Robert Koehler
+#Date: 4/11/15
+
+#Structure
+#1. User Interface:
+    #A. Main Menu
+        #1. Add a New Event
+            #On what date will the Event occur? Press "q" to return to the main menu
+            #What time will the Event occur?
+            #Event Category? (Please type NONE if one is not needed)
+                #*list of categories*
+            #Would you like a reminder?
+                # Yes
+                    #Unit?
+                        #Minute(s)
+                        #Hours(s)
+                        #Days(s)
+                        #Week(s)
+                    #Value?
+                    #Confirmation
+                        #Please confirm the following:
+                            #Yes or No
+        #2. View Today's Schedule
+            #Prints a list of Events with time stamps adjacent to the Event
+            #Press "q" to return to the main menu
+        #3. View Future Events
+            #Please select how you would like to view your Events: Press "q" to return to the main menu
+                #Day
+                #Week
+                #Month
+                #Year
+        #4. Manage Events
+            #Delete Event
+            #Modify Event
+            #Send Event Reminder
+            #Press "q" to return to the main menu
+        #5. Exit Program
+
+#2. Background
+    #A. Automatic Sync with iCloud, Google Apps, Outlook
+    #B. Automatic Basic Encryption
+    #C. List of known contacts
